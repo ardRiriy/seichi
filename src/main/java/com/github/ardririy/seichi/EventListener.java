d578e309b77ce06cb22736630a2075fc70bd7468
ardRiriy <ardririy@protonmail.com>
2022-04-02 11:53:39 +0900

    ブロック破壊数のカウントができるように

--- src/main/java/com/github/ardririy/seichi/EventListener.java
+++ src/main/java/com/github/ardririy/seichi/EventListener.java
@@ -1,15 +1,43 @@
 package com.github.ardririy.seichi;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import java.sql.*;
+import java.util.UUID;
+
 public class EventListener implements Listener{
     @EventHandler
-    public void onBlockBreak(BlockBreakEvent e){
+    public void onBlockBreak(BlockBreakEvent e) throws SQLException {
+
+        //ブロック破壊時にdbを更新
+        final String URL = "jdbc:sqlite:playerDate.db";
+        final String sqlSelect = "select * from player where uuid = ?";
+        final String sqlUpdate = "update player set digging = ? where uuid = ?";
+
         Player player = e.getPlayer();
-        Bukkit.broadcastMessage(player + "さんがブロックを破壊しました");
+        UUID uuid = player.getUniqueId();
+
+        try(Connection conn = DriverManager.getConnection(URL)){
+            conn.setAutoCommit(false);
+
+            try(PreparedStatement p = conn.prepareStatement(sqlSelect)){
+                p.setString(1, String.valueOf(uuid));
+
+                try (ResultSet rs = p.executeQuery()) {
+                    Integer dig_amount = rs.getInt("digging");
+
+                    try(PreparedStatement ps = conn.prepareStatement(sqlUpdate)){
+                        ps.setInt(1,dig_amount + 1);
+                        ps.setString(2, String.valueOf(uuid));
+
+                        ps.executeUpdate();
+                        conn.commit();
+                    }
+                }
+            }
+        }
     }
 }
