257633a6098622d3e1670365829af5a54dcf7579
ardRiriy <ardririy@protonmail.com>
2022-04-03 20:45:34 +0900

    イベントを受け取るファイルの統合

--- src/main/java/com/github/ardririy/seichi/PlayerListener.java
+++ src/main/java/com/github/ardririy/seichi/PlayerListener.java
@@ -3,6 +3,7 @@ package com.github.ardririy.seichi;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.UUID;
@@ -11,6 +12,16 @@ public class PlayerListener implements Listener {
     public PlayerListener(Seichi plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
 
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e){
+
+        Player player = e.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        ConnectDB cdb = new ConnectDB();
+        cdb.atBlockBrake(String.valueOf(uuid));
     }
 
     @EventHandler
--- src/main/java/com/github/ardririy/seichi/Seichi.java
+++ src/main/java/com/github/ardririy/seichi/Seichi.java
@@ -1,21 +1,13 @@
 package com.github.ardririy.seichi;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-
 
 
 public final class Seichi extends JavaPlugin {
 
-    private static Plugin plugin;
     @Override
     public void onEnable() {
         new PlayerListener(this);
-
-        //Event
-        plugin = this;
-        Bukkit.getServer().getPluginManager().registerEvents(new EventListener(), this);
 
         //Command
         getCommand("grading").setExecutor(new grading());
