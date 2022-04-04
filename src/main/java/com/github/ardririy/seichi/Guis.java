d5ec4cc8a6b7de3266069609be91c2cd4171b116
ardRiriy <ardririy@protonmail.com>
2022-04-04 21:37:28 +0900

    コマンドを追加

--- src/main/java/com/github/ardririy/seichi/Guis.java
+++ src/main/java/com/github/ardririy/seichi/Guis.java
@@ -1,16 +1,76 @@
 package com.github.ardririy.seichi;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Guis implements Listener {
 
     private Inventory inv;
 
-    public void openTrashBox(final HumanEntity ent) {
+    public void TrashBox(final HumanEntity ent){
         inv = Bukkit.createInventory(null,54,"TrashBox");
         ent.openInventory(inv);
     }
+
+    public void Toolbox(final HumanEntity ent) {
+        inv = Bukkit.createInventory(null, 9, "Example");
+
+        initializeItems();
+
+        ent.openInventory(inv);
+
+    }
+    public void enchant(ItemStack item){
+        Map<Enchantment,Integer> map = new HashMap<>();
+        map.put(Enchantment.DIG_SPEED, 4);
+        map.put(Enchantment.DURABILITY, 10);
+        item.addUnsafeEnchantments(map);
+    }
+    // You can call this whenever you want to put the items in
+    public void initializeItems() {
+        //ここもうちょっときれいにしたい
+        ItemStack pickaxe = new ItemStack(Material.DIAMOND_PICKAXE);
+        ItemStack axe = new ItemStack(Material.DIAMOND_AXE);
+        ItemStack shovel = new ItemStack(Material.DIAMOND_SPADE);
+        ItemStack  scissor = new ItemStack(Material.SHEARS);
+        enchant(pickaxe);
+        enchant(axe);
+        enchant(shovel);
+        enchant(scissor);
+
+        inv.addItem(createGuiItem(pickaxe, "DIAMOND_PICKAXE"));
+        inv.addItem(createGuiItem(axe,"DIAMOND_AXE"));
+        inv.addItem(createGuiItem(shovel,"DIAMOND_SPADE"));
+        inv.addItem(createGuiItem(scissor,"SHEARS"));
+
+    }
+
+    // Nice little method to create a gui item with a custom name, and description
+    protected ItemStack createGuiItem(final ItemStack item, final String name, final String... lore) {
+        final ItemMeta meta = item.getItemMeta();
+
+        // Set the name of the item
+        meta.setDisplayName(name);
+
+        // Set the lore of the item
+        meta.setLore(Arrays.asList(lore));
+
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
 }
+
+
+
--- src/main/java/com/github/ardririy/seichi/grading.java
+++ src/main/java/com/github/ardririy/seichi/grading.java
@@ -12,22 +12,32 @@ public class grading implements CommandExecutor {
         //nullチェック
         String subCmmand = args.length == 0 ? "" : args[0];
         ConnectDB cdb = new ConnectDB();
+        Guis guis = new Guis();
         Player player = (Player) sender;
+
         if(subCmmand.equalsIgnoreCase("ranking")) {
             //整地数ランキング上位5名を表示
+            //Todo:実行者のランキングも表示できるようにする.
             sender.sendMessage(cdb.dig_ranking());
 
         }else if(subCmmand.equalsIgnoreCase("trash")) {
             //インベントリを閉じた際にアイテムが消滅するゴミ箱を表示
-            Guis guis = new Guis();
-            guis.openTrashBox((HumanEntity) sender);
-        }else if(subCmmand.equalsIgnoreCase("status")){
+            guis.TrashBox((HumanEntity) sender);
+
+        }else if(subCmmand.equalsIgnoreCase("status")) {
+            //実行したプレイヤーの採掘量を表示.
+            //Todo:実行者のRankingも同時に表示できるようにする.
             sender.sendMessage(sender.getName() + " : " + cdb.getDigAmount(String.valueOf(player.getUniqueId())) + "blocks");
+
+        }else if(subCmmand.equalsIgnoreCase("tool")){
+            //コマンド実行 -> Tool 配布
+            guis.Toolbox(player);
 
         }else{
             sender.sendMessage("/grading ranking - Show ranking");
             sender.sendMessage("/grading trash - Open trash box");
             sender.sendMessage("/grading status - Show your status");
+            sender.sendMessage("/grading tool - Get new tools.");
         }
         return false;
     }
